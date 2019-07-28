6a0e136c5543cbf1d7fb51ab6a9db7ba90110d51
Anton Shumikhin <shumih.anton@gmail.com>
2019-07-28 08:52:43 +0300

    234

--- hackaton-lift-client/src/environments/environment.ts
+++ hackaton-lift-client/src/environments/environment.ts
@@ -1,25 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCvd7243WYDIg1n61DiUu5HNv1t0er0yuE',
-    authDomain: 'hackdb-52f72.firebaseapp.com',
-    databaseURL: 'https://hackdb-52f72.firebaseio.com',
-    projectId: 'hackdb-52f72',
-    storageBucket: '',
-    messagingSenderId: '1051457209207',
-    appId: '1:1051457209207:web:1425aa09043f8807',
-  },
+  
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
