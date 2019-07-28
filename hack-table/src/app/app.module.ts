b9912a880a88b30c16dc31a02035ea39e13a8680
Anton Shumikhin <shumih.anton@gmail.com>
2019-07-28 13:56:06 +0300

    phone z-index

---
+++ hack-table/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
